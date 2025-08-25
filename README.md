fb2b4e21c24d8713fdca2b89db5572d3dacffdff
Charis S <anacharism@gmail.com>
2025-08-25 08:13:42 +0000

    edited death messages, commented

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hunger-games-tribute-simulator
 Unfinished version of the hunger games tribute simulator i guess??
+I am planning to use python and pyscript in html to view it from a browser. 
+Expect this to be completed by november 26th or never.
--- core_engine.py
+++ core_engine.py
@@ -2,15 +2,27 @@ import random
 tributes = []
 death_messages = [
     ' slipped on wet moss while running and snapped pronoun neck.',
-    ' somehow died.'
-    
+    ' somehow died.',
+    ' died mysteriously.',
+    ' fell out of a tree trying to spy on person.',
+    ' drank carelessly from a stream without checking, started flailing for a minute or so, and died.'
+    ' burned pronoun_self to death while trying to start a campfire as the gasoline exploded in their face.',
+    ' tried to climb a tree with a bow in their mouth and promptly choked to death.',
+    ' got crushed under a parachute drop from District number they were too excited to catch.',
+    ' has a drink with name_2 and poisons the drink but forgets which one pronoun poisoned.',
+    ' fell asleep in 6 7 foot tall grass and got trampled by mutts.',
+    ' died after being too skibidi.'
+    ' was killed by a convienient Lebron James holding an axe.'
+    ' lost all pronoun aura. '
     ]
+#find tribute from their name (no strings involved)
 def find_tribute(name):
     for tribute in tributes:
         if tribute["name"] == name:
             return tribute
         return None
 
+# get the number of tributes at the beginning
 def num_tributes(number):                                #tribute list
     for tribute in range(number):
         name = input('Tribute Name: ')
@@ -19,6 +31,7 @@ def num_tributes(number):                                #tribute list
         tributes.append(tribute)
     return tributes
 
+#randomly select death message (and variables)
 def select_death_message(tribute):
     tribute_name = find_tribute(tribute)
     tribute_name["status"] = "dead"
@@ -27,13 +40,27 @@ def select_death_message(tribute):
     else:
         pronoun = "him"
     message = random.choice(death_messages)
-    message = message.replace("their", pronoun)
+    if "pronoun" in message:
+        message = message.replace("pronoun", pronoun)
+    elif "pronoun_self" in message:
+        pronoun = pronoun + "self"
+        message= message.replace("pronoun_self", pronoun)
+    elif "name_2" in message:
+        name_2 = random.choice(tributes)
+        while name_2["name"] == tribute_name["name"]:
+            name_2 = random.choice(tributes)
+        message = message.replace("name_2", name_2["name"]) #check this
+    elif "number" in message:
+        number = random.randint(1, 12)
+        message = message.replace("number", str(number))
     return tribute_name["name"] + message
 
+#selects close to death message
 def select_close_to_death_message(tribute):
     if tribute["name"] == tribute:
         None
 
+#opening function bloodbath: how many should die?
 def bloodbath():    
     for tribute in tributes:
         chance = random.randint(1, 4)
@@ -45,15 +72,14 @@ def bloodbath():
             output_ = select_death_message(find_name) + output_ #here
                 
 
-
-  
-
-#randomiser core engine
+#randomiser core engine, place all code in here later
 def game_one():
     None
 
+#first user input** check pyscript
 number_of_tributes = int(input('Enter the number of tributes: ')) 
 
+#checks if the tribute is alive and location.
 for tribute in tributes:
     if tribute["status"] == "alive":
         if tribute["place"] == "cornucopia":
