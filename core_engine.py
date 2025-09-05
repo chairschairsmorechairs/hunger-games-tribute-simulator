f44981b7a1ab681120bf38c86695b1683251bd6a
Charis S <anacharism@gmail.com>
2025-09-05 05:31:23 +0000

    more functions, resolved double function

--- core_engine.py
+++ core_engine.py
@@ -1,5 +1,4 @@
 import random
-from pyscript import Element #investigate why this isnt working and how to actually output to html
 
 print('Welcome to the Hunger Games Tribute Simulator!')
 tributes = [] # do i need this?
@@ -10,19 +9,23 @@ death_messages = [
     ' fell out of a tree trying to spy on name_2.',
     ' drank carelessly from a stream without checking, started flailing for a minute or so, and died.'
     ' burned pronoun_self to death while trying to start a campfire as the gasoline exploded in their face.',
-    ' tried to climb a tree with a bow in their mouth and promptly choked to death.',
-    ' got crushed under a parachute drop from District number they were too excited to catch.',
+    ' tried to climb a tree with a bow in their mouth and promptly choked to death.', #this too..
+    ' got crushed under a parachute drop from District number they were too excited to catch.', #idk about thiss
     ' has a drink with name_2 and poisons the drink but forgets which one pronoun poisoned.',
     ' fell asleep in 6 7 foot tall grass and got trampled by mutts.',
     ' died after being too skibidi.',
     ' was killed by a convienient Lebron James holding an axe.',
-    ' lost all pronoun aura. ', #idk bout this one
-    ' died in shock after losing the huzz.',
+    ' caused name_2 to lose all their aura, effectively killing pronoun. ', #idk bout this one
+    ' died in shock after losing the huzz.', #alliance?
     ' tried to battle name_2 and failed miserably, only managing to splatter a single item on them before dying.', #replace item
     ' became allergic to the air and died.',
     ' literally just died.',
-    ' died from looking at name_2, who was chopped.', # meaning?
-
+    ' died from looking at name_2, who was too chopped to look at.', # meaning?
+    ' was killed when a coconut dropped on their head.',
+    ' ate too much sand and perished.',
+    ' was stung bay wasps', #finish this sentence
+    ' was eaten alive by name_2',
+    
     ]
 
 #night messages etc
@@ -33,6 +36,7 @@ night_messages = [
     ' sleeps in a tree.',
     ' hears a sound in the night decision', #not done
     ' found a square hole in the ground and decision', #not done' #group similar ones?
+    ' hears voices in the night which tells them to action decision', #they are crazy. insert action
 ]
 
 #alliance messages, add more, run chance, what is the chance they get an alliance
@@ -46,6 +50,7 @@ alliance_messages = [
 near_death_messages = [
     ' was almost run over by a stray Pinnochio but escaped by a hair.',
     ' found poisonous berries, decision.'
+    #add falling out of a tree? or no?
 ]
 
 #not even half done
@@ -59,20 +64,46 @@ it_was_coming_death_messages = [ #can i add this to death messages?
 #chances they fight etc
 fight_messages = [
     ' started beefing with name_2 over a cob of corn verdict',
+    ' saw name_2 was following them and fired an arrow at them, verdict',
+    ' jumped out of a bush and attacked name_2, verdict', # add more; not done
+    ]
+
+cornucopia_messages = [
+	' decides to quickly run to the cornucopia and grab and item outside', # just do random chance of this??
+	' decides to avoid the cornucopia and hides in the forest.',
+	' decides to risk it and grab the weapon from the heart of the cornucopia, decision', #add decision and chance of death'
+    ' fights name_2 over a machete in the cornucopia, verdict', #add more
 ]
+
+USE_BROWSER = False  # change this to True when using PyScript
+
+if USE_BROWSER:
+    from js import document
+    def log_event(message: str):
+        output_div = document.getElementById("game-output")
+        output_div.innerHTML += message + "<br>"
+        output_div.scrollTop = output_div.scrollHeight
+else:
+    def log_event(message: str):
+        print(message)
+
 
 #find tribute from their name (no strings involved)
 def find_tribute(name):
     for tribute in tributes:
         if tribute["name"] == name:
             return tribute
-        return None
+        
+def add_kills_and_find(tribute):
+    tribute["kills"] += 1
+    return tribute["kills"]
 
 # get the number of tributes at the beginning
 def num_tributes(number):                                #tribute list
     for tribute in range(number):
-        name = input('Tribute Name: ')
-        tribute = {"name": "Kaitlyn", "gender": "f", "weapons": ["none", "none"], "health": "full", "place": "n/a", "items": ["none", "none"], "status": "alive", "kills": "0",}
+        name = input('Tribute Name: ') #fix inputs
+
+        tribute = {"name": name, "gender": "f", "weapons": ["none", "none"], "health": "full", "place": "n/a", "items": ["none", "none"], "status": "alive", "kills": 0,}
         
         tributes.append(tribute)
     return tributes
@@ -83,10 +114,11 @@ def select_death_message(tribute):
     tribute_name["status"] = "dead"
     message = random.choice(death_messages)
     output = find_pronoun(tribute_name, message)
+    log_event(output)
     return output
 
 #finds pronoun and replaces variables
-def find_pronoun(tribute, message):
+def find_pronoun(tribute, message):# resolve other inputs
     if tribute["gender"] == "f":                       
         pronoun = "her"
     else:
@@ -100,6 +132,7 @@ def find_pronoun(tribute, message):
         name_2 = random.choice(tributes)
         while name_2["name"] == tribute["name"]:
             name_2 = random.choice(tributes)
+        add_kills_and_find(name_2["name"])
         message = message.replace("name_2", name_2["name"]) #check this
     elif "number" in message:
         number = random.randint(1, 12)
@@ -109,39 +142,71 @@ def find_pronoun(tribute, message):
             decision = random.choice(['but decided it was too dangerous and ran away.', 'and slept peacefully until they realised they were stuck inside like Winnie the Pooh.'])
         elif message == near_death_messages[1]:
             decision = random.choice(['and decided to keep them for later', ''])
+        elif message == night_messages[2]:
+            chance = random.randint(1, 4)
+            if chance >= 3:
+                decision = 'but decided to ignore it and go back to sleep.'
+            elif chance == 2:
+                decision = 'and decided to investigate, but by the time they reached the site, it was nothing.'
+            else:
+                name_2 = random.choice(tributes)
+                while name_2["name"] == tribute["name"]:
+                    name_2 = random.choice(tributes)
+                decision = 'and decided to investigate, finding' + name_2["name"] + 's camp.' #add alliance or death instantly
         message = message.replace("decision", decision)
     return tribute["name"] + message
+
+def select_cornucopia_msg(tribute):
+    tribute_name = find_tribute(tribute)
+    message = random.choice(cornucopia_messages)
+    output = find_pronoun(tribute, message)
+    log_event(output)
+    return output
 
 #chooses a night message randomly
 def select_night_message(tribute):
     tribute_name = find_tribute(tribute)
-    tribute_name["status"] = "injured"
     message = random.choice(night_messages)
     output = find_pronoun(tribute_name, message)
-    
+    log_event(output)
+    #return output
+
+
 
 #selects close to death message
 def select_close_to_death_message(tribute):
     if tribute["name"] == tribute:
-        None
+         #add more code
+        message = random.choice(near_death_messages)
+        output = find_pronoun(tribute, message)
+        log_event(output)
 
 #opening function bloodbath: how many should die?
-def bloodbath():    
+def bloodbath():
+    #trigger bloodbath effect + screen
     for tribute in tributes:
-        chance = random.randint(1, 4)
-        if chance >= 3:
-            tribute = tribute[tribute]["health"] = "none"
-            tribute = tribute[tribute]["status"] = "dead"
-            tributes.append(tribute) # not sure about this
-            find_name = tribute[tribute]["name"]
-            output_ = select_death_message(find_name) + output_ #here
-# remove from dict
+        if tribute["status"] == "alive" and tribute["place"] == "cornucopia": # or tribute running away
+            chance = random.randint(1, 4)
+            if chance >= 3:
+                tribute["health"] = "none"
+                tribute["status"] = "dead"
+                message = select_death_message(tribute["name"])
                 
+            else:
+                tribute = find_tribute(tribute["name"])
+                message = select_cornucopia_msg(tribute) # add func, could find weapons
+                # TODO: output to HTML instead of print
+                log_event(message)
+                #print(message)
 
-
+# remove from dict
+def remove_tribute_and_cannons(tribute):
+    dead_names = []
+    for tribute in dead_names:
+        tributes = [t for t in tributes if t["name"] != tribute["name"]] #do i need this
 
 #first user input** check pyscript
-number_of_tributes = int(input('Enter the number of tributes: ')) #change input
+number_of_tributes = int(input('Enter the number of tributes: ')) #change input, move to bottom !!!!!!!!!!!
 
 #decides if the tribute goes to cornucopia or runs away
 def choose_location():
@@ -149,76 +214,107 @@ def choose_location():
         None #here do stuff where i left off
         chance = random.randint(2, 6)
         if chance >= 4:
-            tribute["place"] = "cornucopia" 
+            tribute["place"] = "the cornucopia" 
+        elif chance == 3:
+            tribute["place"] = "higher ground" #tribute is leaving to go to high ground. "the forest but plans to go to higher ground"
+        elif chance == 2:
+            tribute["place"] = "the rock face to scale the mountain"
+            #chance of falling off and dying
         else:
-            tribute["place"] = "volcano" #add more places??
+            tribute["place"] = "the forest" #add more places??
+            #print the messages - all of them - have i?
+        log_event(f"{tribute['name']} decides to head to {tribute['place']}.")
 
 #checks if the tribute is alive and location.
-def bloodbath():                            #call after choose location !!!! very important!!! 
+
+
+
+def action():
     for tribute in tributes:
         if tribute["status"] == "alive":
-            if tribute["place"] == "cornucopia":
-                None#bloodbath code         ***blood bath effect/background screen***
-            elif tribute["place"] == "volcano":
-                None# find weapons code different directions or no? different volcano stuff??
-        else:
-            None # how to remove from dict? 
+            chance = random.randint(1, 10)
+            if chance >= 8:
+                alive_ness = random.randint(1, 2)
+                if alive_ness == 2: 
+                    tribute["status"] = "dead"
+                    tribute["health"] = "none"
+                    message = select_death_message(tribute["name"])
+                    log_event(message)
+                #fight code/die
+                #link to death_messages
+                #add kills
+                #add
 
+            elif chance >= 6:
+                None #find item code
+
+            elif chance == 5:
+                pass #food ?
+            elif chance == 2:
+                None #find weapon code
+            else:
+                None #do nothing code
+        else:
+            None #remove from dict? unless done previously
+
+
+#first day function (bloodbath,)
 def day_one():
     None #yet
     choose_location() #reverse cornucopia, make them find items as well
     bloodbath()
+    action()
     #food? 
 
 
 
 def day_miscellaneous():
     choose_location() #not cornucopia
+    action()
 
     None #yet
 
 def night():
     None #yet
-    #night messages
-    winner_yet_query = one_left()   
-    if winner_yet_query == True:
-        for tribute in tributes:
-            if tribute["status"] == "alive":
-                return tribute["name"]  + "is the champion of the 2025 Hunger Games!" #display this!!1
-    else:
-        return False #input "click to proceed", fix
+    #call night messages
+    for tribute in tributes:
+        if tribute["status"] == "alive":
+            message = select_night_message(tribute["name"])
+            #output message to html
+    #input "click to proceed", fix
         #find winner
     #use this -> number_of_shots can be heard in the distance. The following tributes have fallen:
     #cannon shots fired DO THIS
     #list dead tributes
 
 def one_left():
-    for tribute in tributes:
-        if tribute["status"] == "alive":
-            return False 
-        else:
-            return True
+    alive_count = sum(1 for tribute in tributes if tribute["status"] == "alive")
+    return alive_count <= 1
 
 def find_winner():
     for tribute in tributes:
         if tribute["status"] == "alive":
-            print(tribute["name"] + " is the winner of the Hunger Games!")
+            winner_message = f"{tribute['name']} is the winner of the Hunger Games!"
+            
+            # put message into a <div> with id="game-output"
+            output_div = document.getElementById("game-output")
+            output_div.innerText = winner_message
+            log_event(winner_message)
             return tribute["name"]
-
+        
 #randomiser core engine, place all code in here later
 def one_game():
     day_one()
-    while one_left() == False: #can i put brackets here? check this please
-        None #day/night cycle
+    while not one_left():
         night()
         day_miscellaneous()
-        one_left()
-    print('Would you like to play again?') # make a button
-    #fix this and complete cycle
+    winner = find_winner()
+    log_event(f"{winner} is the winner of the Hunger Games!")
     
     
 
 #one_game()
+#option to repeat()
 
-output_div = Element.querySelector('#test_output') #check if this works
-output_div.innerHTML = "Testing testing 123" #check if this works
+output_div = document.getElementById("test_output")
+output_div.innerText = "Testing testing 123"
